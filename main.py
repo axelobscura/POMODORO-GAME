dc08903b612a6ba214b4cdce22e07cf1d6c8e775
Axel <axosar@gmail.com>
2021-02-26 11:24:55 -0600

    Setting different timer session and value

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
@@ -16,17 +16,21 @@ reps = 0
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
     global reps
+    reps += 1
+
     work_seg = WORK_MIN * 60
     short_break_seg = SHORT_BREAK_MIN * 60
     long_break_seg = LONG_BREAK_MIN * 60
 
-    count_down(work_seg)
-
-    count_down(long_break_seg)
-
-    count_down(short_break_seg)
-
-    count_down(5 * 60)
+    if reps % 8 == 0:
+        count_down(long_break_seg)
+        title_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_seg)
+        title_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_seg)
+        title_label.config(text="Work", fg=GREEN)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
@@ -39,6 +43,8 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_seg}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
